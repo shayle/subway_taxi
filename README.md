e5fc8b1d9ad394a01b9508c61810fcbdc5c30e22
Shay <lehmann.shay@gmail.com>
2017-05-10 20:45:58 -0400

    Adding final pyspark script + analysis notebook + updated README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Project quantifying the effect of subway delays on taxi ridership
+
+taxi_categorizing.py: To be run on HPC cluster. Script uses PySpark to read in taxi data for June 2016. Assigns each trip to an date, hour and subway station if within 300 foot radius. Points not adjacent are dropped. Returns csv with containing a ride count for each date, time and station. 
+
+Ridership_with_Delay.ipynb: Merges taxi data with subway delay data and generates plots + runs ttest.  
+
+
--- taxi_categorizing.py
+++ taxi_categorizing.py
@@ -13,11 +13,14 @@ sc = pyspark.SparkContext()
 sqlContext = pyspark.sql.SQLContext(sc)
 
 # read in desired taxi file
-yellow = sc.textFile('/Users/shay/CUSP/BDM/subway_taxi_project/data/yellow_tripdata_2016-06.csv')
-
+yellow = sc.textFile('/user/sl5335/subway_taxi_project/data/yellow_tripdata_2016-06.csv')
+green = sc.textFile('/user/sl5335/subway_taxi_project/data/green_tripdata_2016-06.csv')
+header = green.first()
+green.filter(lambda x: x != header)
+yellow = yellow.union(green.filter(lambda x: x != header)) 
 
 # read in subway stations
-subwayst = gpd.read_file('/Users/shay/CUSP/BDM/subway_taxi_project/data/subwaystations/subwaystations.shp')
+subwayst = gpd.read_file('/home/sl5335/subway_taxi_project/data/subwaystations/subwaystations.shp')
 subwayst.set_geometry('geometry', inplace = True, crs = 4326)
 subwayst.crs = from_epsg(4326)
 subwayst = subwayst.to_crs(epsg = 2263)
@@ -26,7 +29,7 @@ subwayst = subwayst.to_crs(epsg = 2263)
 # notee - wanted to use entrances but couldn't find file mapping
 # entrances to stations
 
-subwayst['buffer'] = subwayst.geometry.apply(lambda x: x.buffer(100))
+subwayst['buffer'] = subwayst.geometry.apply(lambda x: x.buffer(300))
 
 # remove Staten Island (no SIRee)
 subwayst = subwayst[subwayst.line != 'SIR']
@@ -55,31 +58,23 @@ def extractGeom(partId, records):
     reader = csv.reader(records)
     for row in reader:
         # extract longit & latit and convert to feet
-        longit = float(row[5])
-        latit = float(row[6])
-        geom = Point(proj(longit, latit))
-        match = index.intersection((geom.x-200, geom.y-200, geom.x+200,
-                geom.y+200))
-        nearest = (1e6, None)
-        for idx in match:
-            nearest = min(nearest, (geom.distance(subwayst.geometry[idx]), idx))
+        if len(row) > 8: 
+		longit = float(row[5])
+        	latit = float(row[6])
+        	geom = Point(proj(longit, latit))
+        	match = index.intersection((geom.x-300, geom.y-300, geom.x+300,
+                	geom.y+300))
+        	nearest = (1e6, None)
+        	for idx in match:
+           		nearest = min(nearest, (geom.distance(subwayst.geometry[idx]), idx))
         #(datetime, geom) = (row[1], (float(row[5]), float(row[6])))
         #geom = Point(float(row[5]), float(row[6]))
-        datetime = row[1]
-        date = datetime[8:10]
-        hour = datetime[11:13]
-        yield ((int(date), int(hour)), subwayst.objectid[nearest[1]])
-
-uniqueids = [str(i) for i in subwayst.objectid.unique()]
-
-def createCounts(value_list):
-    import numpy as np
-    value_list = np.array(value_list)
-    station_dict = {}
-    for station in uniqueids:
-        count = len(value_list[value_list == station])
-        station_dict[station] = count
-    return station_dict
+        	if nearest != (1e6, None):
+			datetime = row[1]
+			date = datetime[8:10]
+        		hour = datetime[11:13]
+        		if (int(hour) > 6) and (int(hour) < 21): 
+				yield ((int(date), int(hour)), subwayst.objectid[nearest[1]])
 
 # map partitions to date/hour key
 # filter to between 7 am & 8 pm
@@ -88,7 +83,11 @@ def createCounts(value_list):
 # collect and save to be merged with subway delay data based on time and station
 from collections import Counter
 small_yellow = yellow.mapPartitionsWithIndex(extractGeom) \
-                .filter(lambda x: (x[0][1] > 6) and (x[0][1] < 22) \
-                .map(lambda x: (x[0], [str(x[1])])) \
-                .reduceByKey(lambda x, y: x + y) \
-                .mapValues(lambda x: (Counter(x))).collect()
+		.map(lambda x: (x[0], [str(x[1])])) \
+                .reduceByKey(lambda x, y: x + y, numPartitions = 128) \
+                .mapValues(lambda x: Counter(x))
+results = small_yellow.collect()
+
+df = pd.DataFrame.from_records(results)
+small_yellow.saveAsTextFile('/user/sl5335/subway_taxi_project/results2.txt')
+df.to_csv('/home/sl5335/subway_taxi_project/results.csv')
